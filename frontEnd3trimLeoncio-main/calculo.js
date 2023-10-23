18d93220617fde905cce5d3ceb6d46ad8b562d6c
eduardosantosp <130381385+eduardosantosp@users.noreply.github.com>
2023-10-23 08:57:45 -0300

    Add files via upload

---
+++ frontEnd3trimLeoncio-main/calculo.js
@@ -0,0 +1,15 @@
+function Calcular(){
+
+    let idadeCanina = prompt("Qual a idade do cachorrinho?");
+    let idadeHumana = prompt("Qual a idade Humana?");
+
+    
+
+    mostraResultado.textContent = "Resultado: a idade humana do cachorrinho é " + idadeCanina;
+
+
+    //BRO ESTOU PERDIDO NÃO TA FAZENDO SOL, VOCÊ PODE ME AJUDAR A TERMINAR ESSA CALCULADORA PARA MEU FILHO??
+    //POR FAVOR BRO TERMINE PARA MIM GANG GANG GANG 777 FERNANDX CLOATHING
+    
+    //TO PIOR BRO FODEU TA NUBLADO JÁ KKKK 
+}
